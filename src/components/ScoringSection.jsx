181fe9264c676315b3b60bfe3401297b0fd67af2
ujjwal738 <112887468+ujjwal738@users.noreply.github.com>
2025-01-02 03:06:38 +0530

    Update ScoringSection.jsx

--- src/components/ScoringSection.jsx
+++ src/components/ScoringSection.jsx
@@ -1,6 +1,24 @@
+// import React from "react";
+
+// const ScoringSection = ({ scoring }) => (
+//   <div className="response-section">
+//     <h2>Scoring</h2>
+//     <ul>
+//       {Object.entries(scoring).map(([key, value]) => (
+//         <li key={key}>
+//           {key}: {value}
+//         </li>
+//       ))}
+//     </ul>
+//   </div>
+// );
+
+// export default ScoringSection;
+
+
 import React from "react";
 
-const ScoringSection = ({ scoring }) => (
+const ScoringSection = ({ scoring, fileSize, dimensions}) => (
   <div className="response-section">
     <h2>Scoring</h2>
     <ul>
@@ -9,6 +27,8 @@ const ScoringSection = ({ scoring }) => (
           {key}: {value}
         </li>
       ))}
+      <li>File Size: {fileSize} KB</li>
+      <li>Dimensions: {dimensions.width} X {dimensions.height} pixels</li>
     </ul>
   </div>
 );
